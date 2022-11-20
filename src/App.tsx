10722c15c70f56615a4bf89e67561ba826e32322
Roberto Iglesias Cuesta <ricuesta1986@gmail.com>
2022-11-20 15:53:27 -0500

    Dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
+import {useColorMode, Button, Stack, Box} from "@chakra-ui/react";
+import {DndProvider} from "react-dnd";
+import {HTML5Backend} from "react-dnd-html5-backend";
+
 function App() {
+  const {colorMode, toggleColorMode} = useColorMode();
+
   return (
     <div className="App">
       Todo
       {/* <!-- Add dynamic number --> items left */}
-      All Active Completed Clear Completed Drag and drop to reorder list
+      {`All \n Active \n Completed \n Clear Completed \n Drag and drop to reorder list`}
+      <header>
+        <Button onClick={toggleColorMode}>Toggle {colorMode === "light" ? "Dark" : "Light"}</Button>
+      </header>
+      <DndProvider backend={HTML5Backend}>
+        <Stack>
+          <Box>1</Box>
+          <Box>2</Box>
+          <Box>3</Box>
+        </Stack>
+      </DndProvider>
     </div>
   );
 }
